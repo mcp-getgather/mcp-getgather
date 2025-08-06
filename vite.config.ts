6c00e64e815e9b15e6da8a33c0c998d351403446
broerjuang <juangwiantoro@gmail.com>
2025-08-06 15:56:22 +0700

    fix: image path on start and link

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       "^/(brands|link|parse|start|auth|replay|static)|^/$": {
-        target: "http://localhost:8000",
+        target: "http://127.0.0.1:8000/",
         changeOrigin: true,
       },
     },
