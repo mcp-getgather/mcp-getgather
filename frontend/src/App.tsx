c07dfa88e01755035b1c7a588e9cb9375c4a21df
broerjuang <juangwiantoro@gmail.com>
2025-08-08 07:46:59 +0700

    feat: add shadcn + tailwind

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  );
 }
