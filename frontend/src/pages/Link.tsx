286b6bba11700877f3908a6e6e477a3afa2bd75a
Faisal <faisal@heyario.com>
2025-08-26 16:32:54 +0800

    fix test

--- frontend/src/pages/Link.tsx
+++ frontend/src/pages/Link.tsx
@@ -104,7 +104,7 @@ export default function Link() {
       onUpdateStatus={handleUpdateLinkStatus}
       ref={brandFormRef}
       onSuccess={handleSuccess}
-      successMessage="Authentication successful!\nYou can go back to the app now."
+      successMessage={`Authentication successful!\nYou can go back to the app now.`}
     />
   );
 }
