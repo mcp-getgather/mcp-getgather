f4586d1a675a6af99a69b6fe4dec7ba1ef13ebe5
scoutcallens <scout@getgather.com>
2025-09-12 13:09:18 -0600

    tests working

--- tests/distillation/test_distill.py
+++ tests/distillation/test_distill.py
@@ -1,8 +1,11 @@
-from getgather.distill import distill, load_distillation_patterns
+import os
+import urllib.parse
+
+import pytest
+
 from getgather.browser.profile import BrowserProfile
 from getgather.browser.session import browser_session
-import os
-import pytest
+from getgather.distill import distill, load_distillation_patterns
 
 
 @pytest.mark.asyncio
@@ -10,9 +13,13 @@ async def test_distill():
     profile = BrowserProfile()
     path = os.path.join(os.path.dirname(__file__), "patterns", "**/*.html")
     patterns = load_distillation_patterns(path)
-    print(patterns)
-    assert patterns
+    assert patterns, "No patterns found to begin matching."
+    location = "http://localhost:5001/auth/email-and-password"  # hardcoded for now, will be parameterized later
     async with browser_session(profile) as session:
         page = await session.page()
-        match = await distill("http://localhost:5001/auth/email-and-password", page, patterns)
-        assert match
+        hostname = urllib.parse.urlparse(location).hostname
+        await page.goto(location)
+
+        match = await distill(hostname, page, patterns)
+        assert match, "No match found when one was expected."
+        assert match.name.endswith("acme_email_and_password.html"), "Incorrect match name found."
