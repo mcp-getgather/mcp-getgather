627d61723a0767c6cd91aff9258c1b265a11f66f
Faisal <faisal@heyario.com>
2025-09-15 13:09:14 +0800

    refactor: remove deprecated FE file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,13 +8,11 @@ import "./index.css";
 import { activitiesEnabled, liveViewEnabled, replayEnabled } from "./lib/config";
 import Activities from "./pages/Activities";
 import GetStarted from "./pages/GetStarted";
-import Home from "./pages/Home";
 import Link from "./pages/Link";
 import LiveView from "./pages/LiveView";
 import McpDocs from "./pages/MCPDocs";
 import NotFound from "./pages/NotFound";
 import { ReplayPage } from "./pages/Replay";
-import Start from "./pages/Start";
 
 console.debug(`MULTI_USER_ENABLED: ${import.meta.env.MULTI_USER_ENABLED}`);
 
@@ -23,9 +21,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={new QueryClient()}>
       <BrowserRouter>
         <Routes>
-          <Route path="/home" element={<Home />} />
           <Route path="/link/:linkId" element={<Link />} />
-          <Route path="/start/:brandId" element={<Start />} />
           <Route path="/" element={<Layout />}>
             <Route path="" element={<GetStarted />} />
             {liveViewEnabled() && <Route path="live-view" element={<LiveView />} />}
