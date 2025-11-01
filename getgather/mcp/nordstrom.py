514194d99915761c0b1ee1e02ad73e7abc5be661
Juang Wiantoro <juangwiantoro@gmail.com>
2025-11-01 10:10:16 +0700

    feat(nordstorm): add get_order_history from the network (#594)
    * feat: add get_order_history from the network
    
    * feat: add goto wait commit + simplify nordstrom to use handle network extraction

--- getgather/mcp/nordstrom.py
+++ getgather/mcp/nordstrom.py
@@ -1,8 +1,10 @@
 from typing import Any
 
 from fastmcp import Context
+from patchright.async_api import Page
 
-from getgather.mcp.dpage import dpage_mcp_tool
+from getgather.actions import handle_network_extraction
+from getgather.mcp.dpage import dpage_mcp_tool, dpage_with_action
 from getgather.mcp.registry import GatherMCP
 
 nordstrom_mcp = GatherMCP(brand_id="nordstrom", name="Nordstrom MCP")
@@ -14,4 +16,24 @@ async def get_orders(ctx: Context) -> dict[str, Any]:
 
     return await dpage_mcp_tool(
         "https://www.nordstrom.com/my-account?count=100&page=1&range=all", "nordstrom_orders"
+    )
+
+
+# Currently, no way for us to get the order detail based on the order id since
+# the order id needs to be paired with lookupKey which is not available in the dom / ui
+# so we need to listen specifically to the order details api call
+@nordstrom_mcp.tool
+async def get_order_history(ctx: Context) -> dict[str, Any]:
+    """Get the details of an order from Nordstrom"""
+
+    async def get_order_details_action(page: Page) -> dict[str, Any]:
+        """Get the details of an order from Nordstrom"""
+
+        orders = await handle_network_extraction(page, "/orders")
+
+        return orders
+
+    return await dpage_with_action(
+        "https://www.nordstrom.com/my-account",
+        get_order_details_action,
     )
