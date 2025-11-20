ed7f4058762999f28663cb2cdff1de68cc465c7c
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2025-11-20 11:35:32 +0700

    feat: set browser tz to user tz when provided (#671)

--- getgather/api/types.py
+++ getgather/api/types.py
@@ -10,6 +10,7 @@ class RequestInfo(BaseModel):
     state: str | None = Field(description="The state of the client.", default=None)
     country: str | None = Field(description="The country of the client.", default=None)
     postal_code: str | None = Field(description="The postal code of the client.", default=None)
+    timezone: str | None = Field(description="The timezone of the client.", default=None)
     proxy_type: str | None = Field(
         description="The proxy type to use (e.g., 'proxy-0', 'proxy-1')", default=None
     )
