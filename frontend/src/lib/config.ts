fb81a113316cf61916182f1e12edcb6ea5f4d2f3
broerjuang <juangwiantoro@gmail.com>
2025-11-25 17:46:00 +0700

    refactor(frontend): remove activities and replay pages

--- frontend/src/lib/config.ts
+++ frontend/src/lib/config.ts
@@ -1,11 +1,3 @@
 export function liveViewEnabled() {
   return !import.meta.env.MULTI_USER_ENABLED;
 }
-
-export function activitiesEnabled() {
-  return !import.meta.env.MULTI_USER_ENABLED;
-}
-
-export function replayEnabled() {
-  return !import.meta.env.MULTI_USER_ENABLED;
-}
