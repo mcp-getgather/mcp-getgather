37ad571b5f0045cbcf8e1bdb2ee1c5d2d607e580
rombar <rombar@protonmail.com>
2025-12-22 06:29:28 +0700

    feat: get order done

--- getgather/mcp/alfagift.py
+++ getgather/mcp/alfagift.py
@@ -13,11 +13,27 @@ async def get_cart() -> dict[str, Any]:
     """Get cart alfagift."""
 
     async def action(page: Page, _) -> dict[str, Any]:
+        await page.goto(f"https://alfagift.id/cart")
         data = await handle_network_extraction(page, "active-cart-by-memberId")
-
         return {"alfagift_cart": data["data"]["listCartDetail"]}
 
     return await dpage_with_action(
-        "https://alfagift.id/cart",
+        "https://alfagift.id",
+        action,
+    )
+
+@alfagift_mcp.tool
+async def get_order_done() -> dict[str, Any]:
+    """Get order done alfagift."""
+
+    async def action(page: Page, _) -> dict[str, Any]:
+        await page.goto(f"https://alfagift.id/order-done")
+        data = await handle_network_extraction(page, "order-ereceipt-service/list/complete")
+        return {"alfagift_order_done": data["data"]}    
+        logger.info("Extracted cart data from network response. with data: {data}")
+        return {"alfagift_cart": data["data"]["listCartDetail"]}
+
+    return await dpage_with_action(
+        "https://alfagift.id",
         action,
     )
